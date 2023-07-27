7afd9ace9af2113a7758715ecd1b4cb8d8984726
Patrick Hoydar <pshoydar@gmail.com>
2023-07-27 13:22:39 -0400

    lastest updates that are on staging

--- partials/about-menu.php
+++ partials/about-menu.php
@@ -1,10 +1,10 @@
-<?php if(wp_is_mobile()){ ?>
+<?php if (wp_is_mobile()) { ?>
 	<a href="#" class="right-menu-btn mobile-menu-btn menu-btn" id="about-menu-btn" data-menu-name="about-menu">
 		<span class="open-menu">all</span>
-		<span class="close-menu"><i></i></span>			
-	</a>	
+		<span class="close-menu"><i></i></span>
+	</a>
 <?php } else { ?>
-	
+
 <?php } ?>
 
 <nav class="right-menu" id="about-menu">
@@ -12,8 +12,6 @@
 		<li data-section-name="#timeline2"><a href="#timeline">History</a></li>
 		<li data-section-name="#clients"><a href="#clients">Clients</a></li>
 		<li data-section-name="#people"><a href="#people">People</a></li>
-		<li data-section-name="#culture"><a href="#culture">Culture</a></li>
-		<li data-section-name="#join-us"><a href="#join-us">Join Us</a></li>
+		<li data-section-name="#contact-us"><a href="#contact-us">Contact Us</a></li>
 	</ul>
 </nav>
-
